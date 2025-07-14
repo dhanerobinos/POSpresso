9842da2b01be346e0fc8486a8c6b0da4f3e04f3c
Dhane <ninadhanerobbie.robinos29@gmail.com>
2025-07-14 19:18:29 +0800

    Add User entity

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSpresso.Domain.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+
+    }
+}
