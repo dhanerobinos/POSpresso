568eca91bcda7a6088e19ba61a3a89b598b160f4
Dhane <ninadhanerobbie.robinos29@gmail.com>
2025-07-16 20:58:26 +0800

    Add UserConfig and apply in DbContext

--- Data/POSDbContext.cs
+++ Data/POSDbContext.cs
@@ -1,27 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using POSpresso.Domain.Entities; 
+using POSpresso.Domain.Entities;
+using POSpresso.Infrastructure.Configurations;
 
 namespace POSpresso.Data
 {
-    internal class POSDbContext: DbContext
+    internal class POSDbContext : DbContext
     {
         public DbSet<User> User { get; set; }
-      
+        public POSDbContext(DbContextOptions<POSDbContext> options) : base(options)
+        {
+        }
 
-  
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.ApplyConfiguration(new UserConfig());
+        }
+
+
+
+
         override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(AppContext.BaseDirectory)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
-            optionsBuilder.UseSqlServer(connectionString: configuration.GetConnectionString("DefaultConnection"));
+            if (!optionsBuilder.IsConfigured)
+            {
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(AppContext.BaseDirectory)
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .Build();
+
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+                if (string.IsNullOrEmpty(connectionString))
+                    throw new ArgumentException("Connection string 'DefaultConnection' not found in appsettings.json.");
+
+                optionsBuilder.UseSqlServer(connectionString);
+
+            }
         }
     }
 }
---
+++ Infrastructure/Configurations/UserConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using POSpresso.Domain.Entities;
+namespace POSpresso.Infrastructure.Configurations
+{
+    public class UserConfig: IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.UserId);
+            builder.Property(u => u.Username).IsRequired().HasMaxLength(50);
+            builder.Property(u => u.Password).IsRequired().HasMaxLength(100);
+            builder.Property(u => u.PasswordHash).IsRequired().HasMaxLength(100);
+            builder.Property(u => u.FirstName).IsRequired().HasMaxLength(50);
+            builder.Property(u => u.LastName).IsRequired().HasMaxLength(50);
+            builder.Property(u => u.Role).IsRequired();
+        }
+    }
+}
