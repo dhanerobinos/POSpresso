c70426c8c26634005a430cc5e6ce6d41b227508c
Dhane <ninadhanerobbie.robinos29@gmail.com>
2025-07-21 21:33:03 +0800

    Create form for managing users and Loading Forms

---
+++ Forms/AdminForms/ManageUserForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace POSpresso.Forms.AdminForms
+{
+    partial class ManageUserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 287);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(565, 282);
+            dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(172, 69);
+            button1.Name = "button1";
+            button1.Size = new Size(220, 97);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // UserForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(589, 581);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Name = "UserForm";
+            Text = "UserForm";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button button1;
+    }
+}
---
+++ Services/FormLoaderService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSpresso.Services
+{
+    public class FormLoaderService
+    {
+        public void LoadForm(Panel panel, Form form)
+        {
+            panel.Controls.Clear();
+            form.TopLevel = false;
+            form.FormBorderStyle = FormBorderStyle.None;
+            form.Dock = DockStyle.Fill;
+            panel.Controls.Add(form);
+            panel.Tag = form;
+            form.BringToFront();
+            form.Show();
+        }
+    }
+}
